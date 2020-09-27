857ce61e9480eee3207c0479b8f70a66d4a21d3c
yuyaebean <yuyaebean@gmail.com>
2020-09-27 11:18:25 +0900

    4.2 Testing PATH and DELETE movies id

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -38,7 +38,7 @@ describe('AppController (e2e)', () => {
         .expect([]);
     });
 
-    it('movies (POST)', () => {
+    it('POST 201', () => {
       return request(app.getHttpServer())
         .post('/movies')
         .send({
@@ -47,6 +47,18 @@ describe('AppController (e2e)', () => {
           genres: ['test'],
         })
         .expect(201);
+    });
+
+    it('POST 400', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test',
+          year: 2000,
+          genres: ['test'],
+          other: 'thing',
+        })
+        .expect(400);
     });
   });
 
@@ -61,7 +73,16 @@ describe('AppController (e2e)', () => {
         .get('/movies/999')
         .expect(404);
     });
-    it.todo('DELETE');
-    it.todo('PATCH');
+    it('PATCH 200', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/1')
+        .send({ title: 'Update Test' })
+        .expect(200);
+    });
+    it('DELETE 200', () => {
+      return request(app.getHttpServer())
+        .delete('/movies/1')
+        .expect(200);
+    });
   });
 });
